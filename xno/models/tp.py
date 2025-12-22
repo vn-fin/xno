c01de8084124fdaa3321f9d01536b7a7b9ecff62
kim <kimnt93@gmail.com>
2025-12-22 23:21:55 +0700

    ADD ALL TRADE MODES

--- xno/models/tp.py
+++ xno/models/tp.py
@@ -5,7 +5,7 @@ class TypeTradeMode(enum.StrEnum):
     Test = "test"
     Simulate = "simulate"
     Live = "live"
-    All = "all"
+    Global = "global"
 
 class TypeStage(enum.StrEnum):
     Init = "init"
