545784d020c9109896654cd78138218554279c59
NamNS <namns.dev@gmail.com>
2025-12-25 14:31:20 +0700

    Data2: Fundamental update db name

--- xno/data2/fundamental/external.py
+++ xno/data2/fundamental/external.py
@@ -51,7 +51,7 @@ class WiGroupExternalDataService:
                     industry,
                     sector,
                     sub_industry
-                FROM reference.v_trading_universe
+                FROM reference.trading_universe
                 WHERE symbol = :symbol
                 """
 
@@ -108,7 +108,7 @@ class WiGroupExternalDataService:
                 diluted_eps,
                 audit_firm,
                 audit_opinion
-            FROM reference.v_income_statement
+            FROM reference.income_statement
             WHERE symbol = :symbol
             """
 
